863e3e3772c4b9095efa06e4a04b505234832dba
dimatt45 <dimatt45@yahoo.fr>
2020-03-18 17:21:04 +0100

    TP - Plus ou moins V3 ajout du compteur

---
+++ plusoumoinsV3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main ( int argc, char** argv )
+{
+    int nombreMystere = 0, nombreEntre = 0, nombreCoups =0;
+    const int MAX = 100, MIN = 1;
+
+    // Génération du nombre aléatoire
+
+    srand(time(NULL));
+    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+
+    /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
+
+    do
+    {
+        // On demande le nombre
+        printf("Quel est le nombre ? ");
+        scanf("%d", &nombreEntre);
+        nombreCoups ++;
+
+        // On compare le nombre entré avec le nombre mystère
+
+        if (nombreMystere > nombreEntre)
+            printf("C'est plus !\n\n");
+        else if (nombreMystere < nombreEntre)
+            printf("C'est moins !\n\n");
+        else
+            printf ("Bravo, vous avez trouve le nombre mystere en %d coups !!!\n\n", nombreCoups);
+    } while (nombreEntre != nombreMystere);
+    
+    return 0;
+}
